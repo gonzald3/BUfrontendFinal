db3818d8a38a44f4c1ea8e99147174e8ba242d04
Danny Gonzalez <gonzald3@protonmail.com>
2022-11-30 22:14:10 -0500

    first deploy

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ import Alert from 'react-bootstrap/Alert';
 import background from "../imgs/network.jpg";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import PersonList from './PersonList';
 
 const Home = () => {
 
@@ -38,96 +39,13 @@ const Home = () => {
     loadUser()
   }, [loadUser])
 
-
-
-
-
-
-
-
-
-
-
-
-    const getUserData = async () => {
-        const res = await axios.get("https://arcane-plateau-71865.herokuapp.com/getdata", {
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-
-        if (res.data.status == 201) {
-            console.log("data get");
-            setData(res.data.data)
-
-        } else {
-            console.log("error")
-        }
-    }
-
-
-    const dltUser = async (id) => {
-        console.log(id)
-        const res = await axios.delete(`/${id}`, {
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-
-        if (res.data.status == 201) {
-            getUserData()
-            setShow(true)
-        } else {
-            console.log("error")
-        }
-    }
-
-    useEffect(() => {
-        getUserData()
-    }, [])
-
     
 
     if(userAuth.email === "admin@admin.com"){
         //console.log(userAuth.username);
                 return (
-                    <> 
-                        {
-                            show ? <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-                                User Delete
-                            </Alert> : ""
-                        }
-                        <div className="container mt-2">
-                        <h1>Administrator</h1>
-
-                            
-
-                            <div id="card-div" className="row">
-                                {
-                                    data.length > 0 ? data.map((el, i) => {
-                                        return (
-                                            <>
-                                            <div className="col-12  col-lg-4">
-                                                <Card className="mb-3">
-                                                    <Card.Img variant="top" src={`/uploads/${el.userimg}`} style={{ width: '100px', textAlign: "center", margin: "auto" }} className="mt-2" />
-                                                    <Card.Body className='text-center'>
-                                                        <Card.Title>UserName : {el.username}</Card.Title>
-                                                        <Card.Title>UserName : {el.email}</Card.Title>
-                                                        <Card.Text>
-                                                            Date Added : {moment(el.date).format("DD-MM-YYYY")}
-                                                        </Card.Text>
-                                                        <Button variant="danger" onClick={() => dltUser(el.id)} className='col-lg-6 text-center'>Delete</Button>
-                                                    </Card.Body>
-                                                </Card>
-                                            </div>
-                                            </>
-                                        )
-                                    }) : ""
-                                }
-
-                            </div>
-                        </div>
-                    </>
+                    <PersonList />
+                       
                 );
         }
         else{
---
+++ src/components/PersonList.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import axios from 'axios';
+import Card from 'react-bootstrap/Card';
+import moment from "moment"
+import Button from 'react-bootstrap/Button';
+
+export default class PersonList extends React.Component {
+  state = {
+    persons: []
+  }
+
+   //Must return this functional component in Home.js
+  componentDidMount() {
+    axios.get(`https://arcane-plateau-71865.herokuapp.com/getdata`)
+      .then(res => {
+        const persons = res.data;
+        //console.log(persons)
+        this.setState({ persons });
+      })
+  }
+
+  getData(){
+    axios.get(`https://arcane-plateau-71865.herokuapp.com/getdata`)
+      .then(res => {
+        const persons = res.data;
+        //console.log(persons)
+        this.setState({ persons });
+      })
+  }
+
+  //Must return this functional component in Home.js
+  dltUser = (id, e) => {
+    axios.delete(`/${id}`)
+    .then(res => {
+        console.log(res);
+        const persons = this.state.persons;
+        console.log(res.data);
+        
+        this.getData();
+
+    })
+  }
+  
+//   componentWillMount(){
+//     axios.get('https://arcane-plateau-71865.herokuapp.com/getdata').then(res=>{
+//         const persons = res.data;
+//         this.setState({ state: persons })
+
+//     })
+// }
+  
+
+ //Must return this functional component in Home.js
+  render() {
+    return (
+
+        <div className="container mt-2">
+        <h1>Administrator</h1>
+        <div id="card-div" className="row">
+        {
+          this.state.persons
+            .map(person =>
+                <div className="col-12  col-lg-4">
+                <Card className="mb-3">
+                        <Card.Body className='text-center'>
+                            <div key={person.id}>{person.username}</div>
+                            <Card.Img variant="top" src={`/uploads/${person.userimg}`} style={{ width: '100px', textAlign: "center", margin: "auto" }} className="mt-2" />
+                            <Card.Title>UserName : {person.username}</Card.Title>
+                            <Card.Title>UserName : {person.email}</Card.Title>
+                            <Card.Text>Date Added : {moment(person.date).format("DD-MM-YYYY")}</Card.Text>
+                            <Button variant="danger" onClick={() => this.dltUser(person.id)} className='col-lg-6 text-center'>Delete</Button>
+                    </Card.Body>
+              </Card>
+              </div>
+            )
+        }
+      </div>
+      </div>
+    )
+  }
+}
+
